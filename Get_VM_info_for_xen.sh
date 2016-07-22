0b666aa1c57aecce30f7d35feb174cb1e6276e2a
westporch <westporch@iorchard.co.kr>
2016-07-22 11:27:02 +0900

    Initial

---
+++ Get_VM_info_for_xen.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Hyun-gwan Seo
+
+HOSTNAME=`hostname`
+
+#xl info > xl_info-$HOSTNAME.txt
+
+cat /proc/cpuinfo > cpuinfo-$HOSTNAME.txt
+
+cat /proc/meminfo > meminfo-$HOSTNAME.txt
+free -h > free_h-$HOSTNAME.txt
+
+dmidecode -t memory > dmidecode_memory-$HOSTNAME.txt
+dmidecode -t bios > dmidecode_bios-$HOSTNAME.txt
+dmidecode -t system > dmidecode_system-$HOSTNAME.txt
+
+
+parted -l > parted_l-$HOSTNAME.txt
+df -Th > df_Th-$HOSTNAME.txt
+
+lspci  | grep -i raid > raid-$HOSTNAME.txt
+
+cp /etc/network/interfaces interfaces-$HOSTNAME.txt
+
+#brctl show > brctlshow-$HOSTNAME.txt
+ifconfig > ifconfig-$HOSTNAME.txt
+route > route-$HOSTNAME.txt
+
+lshw > lshw-$HOSTNAME.txt
+dmidecode > dmidecode-$HOSTNAME.txt
+
+dmesg > dmesg-$HOSTNAME.txt
+cat /var/log/kern.log > kern.log-$HOSTNAME.txt
+#dpkg -l > dpkg_l-$HOSTNAME.txt
+lsmod > lsmod-$HOSTNAME.txt
+uname -a > uname_a-$HOSTNAME.txt
+cat /etc/fstab > fstab-$HOSTNAME.txt
+cat /etc/default/grub > grub-$HOSTNAME.txt
+dstat 1 10 > dstat-$HOSTNAME.txt
+
